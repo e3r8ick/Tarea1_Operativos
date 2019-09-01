bf6e75845a3de1b31af3c346388a952e18fe63cc
Michael <michael.gonzalez1812@gmail.com>
2019-09-01 13:48:38 -0600

    makefiles updated

--- daemon-scripts/test.sh
+++ daemon-scripts/test.sh
@@ -2,5 +2,6 @@
 while true
 do
     ./home/mike/Documents/OS/Tarea1/Tarea1_Operativos/bin/main
+    echo funcionando
     sleep 1
 done
--- src/main.c
+++ src/main.c
@@ -1,5 +1,27 @@
-#include <rest.h>
+#include <server.h>
 
-int main(int argc, char *argv[]) {
-    doNothing();
+int main(int c, char** v)
+{
+    serve_forever("1718");
+    return 0;
+}
+
+void route()
+{
+    ROUTE_START()
+
+    ROUTE_GET("/")
+    {
+        printf("HTTP/1.1 200 OK\r\n\r\n");
+        printf("Hello! You are using %s", request_header("User-Agent"));
+    }
+
+    ROUTE_POST("/")
+    {
+        printf("HTTP/1.1 200 OK\r\n\r\n");
+        printf("Wow, seems that you POSTed %d bytes. \r\n", payload_size);
+        printf("Fetch the data using `payload` variable.");
+    }
+
+    ROUTE_END()
 }
