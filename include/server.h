58c101ca4c1282afcb952200f8199ef544558abb
Erick Cordero <eguicoro2@gmail.com>
2019-09-05 23:42:22 -0600

    unify image processing and server

--- include/server.h
+++ include/server.h
@@ -23,14 +23,14 @@ struct connection_info_struct
 };
 
 
-static int send_page (struct MHD_Connection *connection, const char *page);
-static int iterate_post (void *coninfo_cls, enum MHD_ValueKind kind, const char *key,
+int send_page (struct MHD_Connection *connection, const char *page);
+int iterate_post (void *coninfo_cls, enum MHD_ValueKind kind, const char *key,
               const char *filename, const char *content_type,
               const char *transfer_encoding, const char *data, uint64_t off,
               size_t size);
-static void request_completed (void *cls, struct MHD_Connection *connection,
+void request_completed (void *cls, struct MHD_Connection *connection,
                    void **con_cls, enum MHD_RequestTerminationCode toe);
-static int answer_to_connection (void *cls, struct MHD_Connection *connection,
+int answer_to_connection (void *cls, struct MHD_Connection *connection,
                    const char *url, const char *method,
                    const char *version, const char *upload_data,
                    size_t *upload_data_size, void **con_cls);
--- lib/server.c
+++ lib/server.c
@@ -31,8 +31,7 @@ const char *errorpage =
   "<html><body>This doesn't seem to be right.</body></html>";
 
 
-static int
-send_page (struct MHD_Connection *connection, const char *page)
+int send_page (struct MHD_Connection *connection, const char *page)
 {
   int ret;
   struct MHD_Response *response;
@@ -51,8 +50,7 @@ send_page (struct MHD_Connection *connection, const char *page)
 }
 
 
-static int
-iterate_post (void *coninfo_cls, enum MHD_ValueKind kind, const char *key,
+int iterate_post (void *coninfo_cls, enum MHD_ValueKind kind, const char *key,
               const char *filename, const char *content_type,
               const char *transfer_encoding, const char *data, uint64_t off,
               size_t size)
@@ -87,8 +85,7 @@ iterate_post (void *coninfo_cls, enum MHD_ValueKind kind, const char *key,
   return MHD_YES;
 }
 
-static void
-request_completed (void *cls, struct MHD_Connection *connection,
+void request_completed (void *cls, struct MHD_Connection *connection,
                    void **con_cls, enum MHD_RequestTerminationCode toe)
 {
   struct connection_info_struct *con_info = *con_cls;
@@ -111,8 +108,7 @@ request_completed (void *cls, struct MHD_Connection *connection,
 }
 
 
-static int
-answer_to_connection (void *cls, struct MHD_Connection *connection,
+int answer_to_connection (void *cls, struct MHD_Connection *connection,
                       const char *url, const char *method,
                       const char *version, const char *upload_data,
                       size_t *upload_data_size, void **con_cls)
